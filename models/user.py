475278f46047409fb59095fc1e65cdd249ff68c7
Yasser Tahiri <yasserth19@gmail.com>
2021-08-01 01:22:54 +0100

    ⚙ Create User model to Create Table

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from db.base_class import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
